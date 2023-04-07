b7eb2a829ad7f6501d42ede4a27afa359d2c62d3
CodingManoj <mvp2612@hotmail.com>
2023-04-06 22:59:44 -0400

    Git Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,7 +13,8 @@ def lintChecks() {
 def sonarChecks() {
     sh ''' 
         sonar-scanner -Dsonar.host.url=http://172.31.9.236:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
-
+        curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh 
+        bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}
     '''
 }
 
@@ -24,6 +25,7 @@ def call() {
 
         environment { 
             SONAR = credentials('SONAR') 
+            SONAR_URL = "172.31.9.236"
         }
 
         stages {
